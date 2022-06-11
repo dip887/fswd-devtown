333ceac994363ed722bd7d8acfa1519b04cf38a1
dip887 <gdeepanshu289@gmail.com>
2022-06-11 20:46:44 +0530

    request part

--- Readme.md
+++ Readme.md
@@ -1,3 +1,50 @@
-not yet deploy but I have to deploy it
 
-other creative rock paper scissor application:
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.get("/todos", (req, res) => {
+  try {
+    const todos = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "db.json"), { encoding: "UTF-8" })
+    );
+
+    const { count } = req.query;
+
+    if (count) {
+      return res.send(todos.slice(0, count));
+    } else {
+      return res.send(todos.slice(0, count));
+    }
+  } catch (error) {
+    return res.send(error.message);
+  }
+});
+
+app.get("/todos/:id", (req, res) => {
+  try {
+    const todos = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "db.json"), { encoding: "UTF-8" })
+    );
+    const { id } = req.params;
+    const todo = todos.find((e) => e.id == id);
+
+    if (todo) {
+      return res.send(todo);
+    } else {
+      return res.send("item not found");
+    }
+  } catch (error) {
+    return res.send(error.message);
+  }
+});
+//  post part left
+
+app.post
+
+app.listen(8080, () => {
+  console.log(`server started at Port: ${8080}`);
+});
+
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const { Router } = require("express");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
+
+app.use(express.json());
+
+/*
+TYPE: GET
+PARAMS: Null
+QUERY: Count
+DESCRIPTION: route to fetch all todo
+*/
 
 app.get("/todos", (req, res) => {
   try {
@@ -22,11 +32,18 @@ app.get("/todos", (req, res) => {
   }
 });
 
+/*
+TYPE: GET
+PARAMS: id
+QUERY: Null
+DESCRIPTION: route to fetch a specific todo 
+*/
 app.get("/todos/:id", (req, res) => {
   try {
     const todos = JSON.parse(
       fs.readFileSync(path.join(__dirname, "db.json"), { encoding: "UTF-8" })
     );
+
     const { id } = req.params;
     const todo = todos.find((e) => e.id == id);
 
@@ -39,9 +56,101 @@ app.get("/todos/:id", (req, res) => {
     return res.send(error.message);
   }
 });
-//  post part left
 
-app.post
+/*
+TYPE: POST
+PARAMS: Null
+QUERY: Null
+Body: 
+DESCRIPTION: route to add a specific todo
+(also we must add header in this case as content-type)
+(id is auto-assigned by our data structure)
+*/
+
+app.post("/todos", (req, res) => {
+  try {
+    const data = req.body;
+    const todos = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "db.json"), { encoding: "UTF-8" })
+    );
+
+    const todo = {
+      ...data,
+      id: todos.length + 1,
+    };
+
+    todos.push(todo);
+    fs.writeFileSync(path.join(__dirname, "db.json"), JSON.stringify(todos));
+    // console.log(todo);
+    res.send(todo);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+/*
+TYPE: Delete
+PARAMS: id
+QUERY: Null
+Body: 
+DESCRIPTION: route to delete a specific todo
+*/
+
+app.delete("/todos/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const todos = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "db.json"), { encoding: "UTF-8" })
+    );
+
+    //if it's true , its remove the element means filter out
+    const newTodos = todos.filter((todo) => todo.id != id);
+
+    fs.writeFileSync(path.join(__dirname, "db.json"), JSON.stringify(newTodos));
+    // console.log(todo);
+    res.send(newTodos);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
+/*
+TYPE: put
+PARAMS: id
+QUERY: Null
+Body: 
+DESCRIPTION: route to update a specific todo
+*/
+
+app.put("/todos/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const todos = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "db.json"), { encoding: "UTF-8" })
+    );
+
+    const newTodos = todos.map((todo) => {
+      if (todo.id == id) {
+        return {
+          ...todo,
+          ...data,
+        };
+      } else {
+        return todo;
+      }
+    });
+
+    fs.writeFileSync(path.join(__dirname, "db.json"), JSON.stringify(newTodos));
+    // console.log(todo);
+    res.send(newTodos);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
 
 app.listen(8080, () => {
   console.log(`server started at Port: ${8080}`);
